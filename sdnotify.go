674534bb57e1ba3719da33fbab69fdb75284223c
Matt Layher <mdlayher@gmail.com>
2020-06-24 20:16:54 -0400

    sdnotify: document os.ErrNotExist contract
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- sdnotify.go
+++ sdnotify.go
@@ -36,13 +36,18 @@ type Notifier struct {
 }
 
 // New creates a Notifier which sends notifications to the UNIX socket specified
-// by the NOTIFY_SOCKET environment variable.
+// by the NOTIFY_SOCKET environment variable. See Open for more details.
 func New() (*Notifier, error) {
 	return Open(os.Getenv(Socket))
 }
 
 // Open creates a Notifier which sends notifications to the UNIX socket
 // specified by sock.
+//
+// If sock does not exist or is unset (meaning the service is not running under
+// systemd supervision, or is not using systemd unit Type=notify), Open will
+// return an error which can be checked with 'errors.Is(err, os.ErrNotExist)'.
+// Calling any of the resulting nil Notifier's methods will result in a no-op.
 func Open(sock string) (*Notifier, error) {
 	// Don't stat Linux abstract namespace sockets, as would be created with a
 	// net.ListenPacket with no path.
